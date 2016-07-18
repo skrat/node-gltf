9a2ad764cd0525a9e61a4930cf1d618251704380
Dusan Maliarik <dusan.maliarik@gmail.com>
2016-07-18 11:13:46 -0600

    Direct path

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
 var path = require("path");
-module.exports.collada2gltf = path.join(__dirname, "bin", "collada2gltf");
+module.exports.collada2gltf = path.join(__dirname, "glTF", "COLLADA2GLTF", "bin", "collada2gltf");
